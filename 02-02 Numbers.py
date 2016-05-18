d7714d6248c9cefd830a5844f95ef5b2e5c231dc
Rahul Kumar <rahulcomp24@gmail.com>
2016-05-19 01:53:46 +0530

    Add example codes for conditional programs

---
+++ 02-02 Numbers.py
@@ -0,0 +1,10 @@
+number = raw_input("Enter a number : ")
+number = int(number)
+if(number == 1):
+    print "one"
+elif(number == 2):
+    print "two"
+elif(number == 3):
+    print "three"
+else:
+    print "Number is not one, two or three"
---
+++ 02-03 Ternary.py
@@ -0,0 +1,4 @@
+result = "True" if (5>4) else "False"
+print result
+result = "True" if (5>6) else "False"
+print result
