e9c31a70968868455281981e58db03ffb18887d0
Rahul Kumar <rahulcomp24@gmail.com>
2016-05-19 00:37:32 +0530

    Add example codes for expressions and basic syntax

---
+++ 01-01 Basics.py
@@ -0,0 +1,11 @@
+name = raw_input("Enter your name : ")
+print name
+print type(name)
+
+age = int(raw_input("Enter your age : "))
+print age
+print type(age)
+
+number = float(raw_input("Enter a floating number : "))
+print number
+print type(number)
---
+++ 01-02 Basics.py
@@ -0,0 +1,12 @@
+# Accept radius of the circle
+radius = raw_input("Enter radius : ")
+
+#Convert radius from string to float
+radius = float(radius)
+
+'''
+Now to find area we use the given formula
+area = pi * radius * radius
+'''
+area = 3.14 * radius * radius
+print "Area is " + str(area)
