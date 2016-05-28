7f5247e236cd80cd7418e0c65ed02719d4dd1f90
Rahul Kumar <rahulcomp24@gmail.com>
2016-05-29 01:56:50 +0530

    Improved Regular Expression

--- 06-01 WhatsAppChat.py
+++ 06-01 WhatsAppChat.py
@@ -6,7 +6,7 @@ senderList = []
 for line in fhandle:
     line = line.replace("\xe2\x80\xaa","")
     line = line.replace("\xe2\x80\xac","")
-    sender = re.findall('[0-3][0-9]/[0-1][0-9]/1[5-6], [0-9]{1,2}:[0-6][0-9] [AP]M - (.+?):',line)
+    sender = re.findall('[0-3][0-9]/[0-1][0-9]/1[5-6], [0-9]{1,2}:[0-5][0-9] [AP]M - (.+?):',line)
     if(len(sender) > 0):
         if(sender[0] not in senderList):
             senderList.append(sender[0])
